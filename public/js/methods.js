6f6651ec33efbfd2e180a7d52ed8888218e20a4c
Chris Duffy <chrisduffyone@gmail.com>
2016-08-23 13:33:00 +0000

    Progress 823

---
+++ public/js/methods.js
@@ -0,0 +1,9 @@
+//TODO move create js arcade functions here
+
+/*function createChicken(){
+    chickenSprite = new createjs.Sprite(chickenSheet, "flap");
+    chickenSprite.x = 100;
+    chickenSprite.y = 200;
+    chickenSprite.gotoAndPlay("flap");
+    stage.addChildAt(chickenSprite,1);
+}*/
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+var http = require('http');
 var express = require('express');
+var socket_io = require('socket.io');
 
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+//socket io app
+io.on('connection', function(socket){
+   console.log('Person Connect');
+   
+   //enter room
+   socket.on('roomSelect', function(roomNum){
+       socket.join(roomNum);
+       console.log('User enters rm:', roomNum);
+   });
+   
+   //emit to room1
+   setInterval(function(){ 
+      io.in('room0').emit('roomSpecific'); 
+   }, 5000);
+   
+});
 
 console.log('Capstone1 Server is Online');
+server.listen(process.env.PORT || 8080);
 exports.app = app;
